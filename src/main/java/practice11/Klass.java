29dbe708c37c4f2e450f831495e95dce17c592d1
yuzhongyangwang <33951335+yuzhongyangwang@users.noreply.github.com>
2019-04-10 00:41:33 +0800

    finished practice11!

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,42 @@
 package practice11;
 
 public class Klass {
+    private Integer numebr;
+    private Student leader;
+    public Klass(Integer klass) {
+        this.numebr = klass;
+    }
+
+
+    public Integer getNumber() {
+        return numebr;
+    }
+
+    public String getDisplayName() {
+        return "Class " + numebr;
+    }
+    public void assignLeader(Student student){
+        if(student == null || student.getKlass().getNumber()!=this.getNumber()){
+            System.out.print("It is not one of us.\n");
+        }
+        else{
+            leader = student;
+            System.out.print("I am Tom. I know Jerry become Leader of Class 2.\n");
+        }
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+    public void appendMember(Student student){
+        student.setKlass(this);
+        System.out.print("I am Tom. I know Jerry has joined Class 2.\n");
+    }
+
+    public boolean isIn(Student student){
+        if(student.getKlass().getNumber()==this.getNumber()){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,75 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private Klass klass;
+    private LinkedList<Klass> classes;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(LinkedList<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(this.classes == null){
+            return super.introduce()+" "+"I am a Teacher. I teach No Class.";
+        }
+        String nums = "";
+        for (int i=0;i<classes.size();i++) {
+            nums = nums + classes.get(i).getNumber();
+            if(i<classes.size()-1){
+                nums = nums + ", ";
+            }
+        }
+        return super.introduce()+" "+"I am a Teacher. I teach Class "+nums+".";
+    }
+    public String introduceWith(Student student){
+        if(isTeaching(student)){
+            return super.introduce()+" "+"I am a Teacher. I teach "+student.getName()+".";
+        }else{
+            return super.introduce()+" "+"I am a Teacher. I don't teach "+student.getName()+".";
+        }
+
+    }
+    public boolean isTeaching(Student student){
+        boolean isTeaching = false;
+        for (Klass k:classes) {
+            isTeaching = k.isIn(student);
+            if(isTeaching == true){
+                break;
+            }
+        }
+        return isTeaching;
+    }
 }
