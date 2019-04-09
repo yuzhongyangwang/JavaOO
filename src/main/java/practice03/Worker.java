eeb1a34546652d61fd2c64ce3b92670edbea86c1
yuzhongyangwang <33951335+yuzhongyangwang@users.noreply.github.com>
2019-04-09 22:04:25 +0800

    finished practice03!

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,15 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public Worker(){
+
+    }
+    public String introduce(){
+        String Str = "I am a Worker. I have a job.";
+        return Str;
+    }
 }
