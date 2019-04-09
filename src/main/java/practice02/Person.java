00e086f584c389f4ff75e30b441d84a3064e27fd
yuzhongyangwang <33951335+yuzhongyangwang@users.noreply.github.com>
2019-04-09 21:56:55 +0800

    finished practice02!

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,34 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(){
+
+    }
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String introduce(){
+        String Str = "My name is "+name+". I am "+age+" years old.";
+        return Str;
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,22 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name,int age,int klass){
+        this.setName(name);
+        this.setAge(age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    public String introduce(){
+        String Str = "I am a Student. I am at Class "+klass+".";
+        return Str;
+    }
 }
