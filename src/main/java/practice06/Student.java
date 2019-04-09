75fa7f7fe07c11dbde4a6c105112b1222a180239
yuzhongyangwang <33951335+yuzhongyangwang@users.noreply.github.com>
2019-04-09 22:34:57 +0800

    finished practice05!

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name,int age,int klass){
+        this.setName(name);
+        this.setAge(age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    public String introduce(){
+        String Str = super.introduce() + " " + "I am a Student. I am at Class "+klass+".";
+        return Str;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(this.klass == null){
+            return super.introduce()+" "+"I am a Teacher. I teach No Class.";
+        }
+        return super.introduce()+" "+"I am a Teacher. I teach Class "+klass+".";
+    }
 }
